94bc6b3f5081ea1c1134d9532ccb4b581af78442
tail-s <lunar7s@naver.com>
2024-12-15 08:45:23 +0900

    user service : spring security mismatch

---
+++ user-service/src/main/java/com/example/userservice/service/UserServiceImpl.java
@@ -0,0 +1,34 @@
+package com.example.userservice.service;
+
+import com.example.userservice.dto.UserDto;
+import com.example.userservice.entity.UserEntity;
+import com.example.userservice.repository.UserRepository;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
+public class UserServiceImpl implements UserService {
+    @Autowired
+    UserRepository userRepository;
+
+    @Override
+    public UserDto createUser(UserDto userDto) {
+        userDto.setUserId(UUID.randomUUID().toString());
+
+        ModelMapper mapper = new ModelMapper();
+        mapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
+        UserEntity userEntity = mapper.map(userDto, UserEntity.class);
+        userEntity.setEncryptedPwd("encrypted_Pwd");
+
+        userRepository.save(userEntity);
+
+        UserDto returnUserDto = mapper.map(userEntity, UserDto.class);
+
+
+        return null;
+    }
+}
