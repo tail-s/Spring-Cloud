1defd659f94b0677fbe119662693ef3222516607
tail-s <lunar7s@naver.com>
2024-12-25 05:02:18 +0900

    security version : stucked

--- user-service/src/main/java/com/example/userservice/security/WebSecurity.java
+++ user-service/src/main/java/com/example/userservice/security/WebSecurity.java
@@ -1,8 +1,11 @@
 package com.example.userservice.security;
 
+import jakarta.servlet.Filter;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
@@ -14,8 +17,9 @@ public class WebSecurity {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-//                .authorizeHttpRequests(auth -> auth.requestMatchers("/users/**").permitAll())
                 .authorizeHttpRequests(auth -> auth.requestMatchers("/**").permitAll())
+//                .authorizeHttpRequests(auth -> auth.requestMatchers("/users/**").permitAll())
+//                .authorizeHttpRequests(auth -> auth.requestMatchers("/**")).addFilter(getAuthenticationFilter())
                 // h2-console의 default url이 아닌 yml파일에서 수정한 경우에도 접근 가능하도록 열여주기
                 .authorizeHttpRequests(auth -> auth.requestMatchers(PathRequest.toH2Console()).permitAll())
                 .csrf(csrf -> csrf.disable())
@@ -23,5 +27,17 @@ public class WebSecurity {
 
         return http.build();
     }
+
+
+
+//    private Filter getAuthenticationFilter(AuthenticationManager authenticationManager) throws Exception {
+//        return new AuthenticationFilter(authenticationManager);
+//    }
+//
+//    @Bean
+//    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+//        return authenticationConfiguration.getAuthenticationManager();
+//    }
+
 
 }
